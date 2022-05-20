06000a7ab83cead6ad161f852dcc36c99e3fb2ff
Steffen Schultz <steffenschultz@mailbox.org>
2022-05-20 16:04:11 +0200

    Prettify the download website with SimpleCSS

--- web/README.md
+++ web/README.md
@@ -1,8 +1,7 @@
 # Stream download
-A download page for audio stream archives.
+A download and monitoring page for audio stream archives.
 
-This is a simple php script to quickly download a desired stream archive file. It has been created for a microphone input project where stream archives are created for every hour of the day. It of course can be customized for similar projects, e. g. radio show archives.  
-For this script to work The audio files have to be web-accessible. You can place it into the same directory as the audio files and protect the location with a password. 
+This is a simple php script to quickly download a desired stream archive file. It has been created for a microphone input project where stream archives are created for every hour of the day. It of course can be customized for similar projects, e. g. radio show archives. For this script to work The audio files have to be web-accessible. You can place it into the same directory as the audio files and protect the location with a password. 
 
 ## How to configure
 
@@ -15,3 +14,7 @@ Open `config.php` and customize the following settings if needed.
 ## How to download an audio file
 
 Just open `download.php` in your browser and select the desired stream, date and time. Hit the download button to get the file. Your browser probably will start to play the audio, so you might have to right-click the player controls and save the file from there. 
+
+## Credits
+
+Some visual stuff: [Simple.css](https://github.com/kevquirk/simple.css) by Kev Quirk, released under MIT license. 
--- web/download.php
+++ web/download.php
@@ -4,29 +4,32 @@
 <title>Stream Download</title>
 <meta charset="utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1" />
+<link rel="stylesheet" href="https://cdn.simplecss.org/simple.min.css">
 </head>
 <body>
-<div class="content">
+<header>
 <h1>Stream Download</h1>
-<p class="notes">Please select the desired file from the form below and press the download button.</p>
-<div class="download">
+<p>Please select the desired file from the form below and press the download button.</p>
+</header>
+<main>
+<h2>Select archive file</h2>
 <form name="download" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" target="_blank">
 <?php
 require("./config.php");
 if (empty($streamList)) {
-    echo "<p><label for=\"stream\">Stream:</label><br /><input id=\"stream\" name=\"stream\" type=\"text\" /></p>";
+    echo "<p><label for=\"stream\">Stream:</label><br /><input id=\"stream\" name=\"stream\" type=\"text\" /></p>\n";
 } else {
-    echo "<p><label for=\"stream\">Stream:</label><br />";
-    echo "<select id=\"stream\" name=\"stream\">";
+    echo "<p><label for=\"stream\">Stream:</label><br />\n";
+    echo "<select id=\"stream\" name=\"stream\">\n";
     foreach ($streamList as $name => $url) {
-        echo "<option value =\"".htmlspecialchars($name)."\">".ucfirst(htmlspecialchars($name))."</option>";
+        echo "<option value =\"".htmlspecialchars($name)."\">".ucfirst(htmlspecialchars($name))."</option>\n";
     }
-    echo "</select></p>";
+    echo "</select></p>\n";
 }
 ?>
 <p><label for="date">Date:</label><br /><input id="date" name="date" type="text" value="<?php echo htmlspecialchars($dateFormat); ?>" /></p>
 <p><label for="time">Time:</label><br /><input id="time" name="time" type="text" value="<?php echo htmlspecialchars($timeFormat); ?>" /></p>
-<input name="download" type="submit" value="Download" />
+<button name="download" type="submit" value="Download">Download</button>
 <?php
 if(isset($_POST['download'])) {
     $stream = $_POST['stream'];
@@ -36,27 +39,23 @@ if(isset($_POST['download'])) {
         header("Location:{$mediaBase}{$stream}-{$date}_{$time}.mp3");
         exit;
     } else {
-        echo "<p class=\"error\">Please fill all fields!</p>";
+        echo "<p><strong>Please fill all fields!</strong></p>\n";
     }
 }
 ?>
 </form>
-</div>
-<div class="monitor">
 <?php
 if ($streamList) {
-    echo "<h2>Stream Monitor</h2>";
+    echo "<h2>Stream Monitor</h2>\n";
     foreach ($streamList as $name => $url) {
         echo "<h3>".ucfirst(htmlspecialchars($name))."</h3>\n";
-        echo "<div class=\"audio\" role=\"region\" aria-label=\"".htmlspecialchars($name)."\"><audio src=\"".htmlspecialchars($url)."\" controls=\"controls\" preload=\"none\"></audio></div>\n";
+        echo "<audio src=\"".htmlspecialchars($url)."\" controls=\"controls\" preload=\"none\"></audio>\n";
     }
 }
 ?>
-</div>
-<div class="footer">
-<hr>
+</main>
+<footer>
 <p>Created by Steffen Schultz. <a href="https://github.com/schulle4u/wanze">View on GitHub</a>.</p>
-</div>
-</div>
+</footer>
 </body>
 </html>
